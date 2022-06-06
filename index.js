7b75a5af4b01d7fa46ca492cc7e3fa21f6d88653
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-06 10:58:14 -0400

    Grabbing products by there id off of amazon

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
+const apiKey = '9369326f7eeeda5489b984142cd45bf1'
+
+const baseURL = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Welcome to Amazon Web Scrapper API')
+})
+
+// fetch product details
+
+app.get('/products/:productId', async (req, res) => {
+  const { productId } = req.params
+
+  try {
+    const response = await request(`${baseURL}&url=https://www.amazon.com/dp/${productId}`)
+    res.json(response)
+  } catch (error) {
+    res.json(error)
+  }
 })
 
 app.listen(PORT, () => {
